566eb11848b2c5b97b33cb1f15cc1f1878a42e38
ayushsth123 <ayyush227@gmail.com>
2025-03-06 08:04:27 +0545

    hamburger animation testing

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
+import HamburgerAnimation from './HamburgerAnimation'
 
 const Header = () => {
   return (
-    <section className="max-w-[1400px] mt-5 mx-auto w-full">
+    <section className="max-w-[1400px] mt-5 mx-auto w-full flex justify-between items-center">
         <h1 className='nebula uppercase text-lg text-white'>Ayuxh</h1>
+        <Image
+        src='/svgs/hamburger.svg'
+        width={10}
+        height={10}
+        alt='Hamburger image icon'
+        className='w-10 h-10'
+        />
+        <HamburgerAnimation/>
     </section>
   )
 }
