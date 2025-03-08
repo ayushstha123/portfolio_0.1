bb69d8fa4972f1949635bc1bbb756e32f1096f87
ayushsth123 <ayyush227@gmail.com>
2025-03-08 23:39:10 +0545

    ui changes

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <section className="max-w-[1400px] bg-transparent mt-10 mx-auto w-full flex px-5 justify-between items-center">
       <div>
-        <h1 className='nebula uppercase text-sm md:text-lg text-white'>Ayuxh</h1>
-        <div className='flex gap-2 items-center '>
-        <span className='text-green-400 text-sm animate-pulse'>▇ </span>
-        <span className='font-mono font-extralight uppercase text-sm text-white mt-1'>Available for collaboration</span>
+        <h1 className='nebula uppercase text-[10px] sm:text-sm md:text-lg text-white'>Ayuxh</h1>
+        <div className='flex gap-1 md:gap-2 items-center '>
+        <span className='text-green-300 text-sm animate-pulse'>▰</span>
+        <span className='font-mono font-extralight uppercase text-[9px] md:text-sm text-white mt-1 md:mt-0'>Available for collaboration</span>
         </div>
         </div>
         <div className='flex flex-col gap-1 group cursor-pointer'>
--- app/components/sections/HomeSection/HeroSection.jsx
+++ app/components/sections/HomeSection/HeroSection.jsx
@@ -23,24 +23,25 @@ const HeroSection = () => {
                     pointerEvents: 'none',
                     transition: ' 0.1s ease'
                 }}
+                className='md:block hidden'
             />
             <section className=" max-w-[1400px] mx-auto px-2 flex items-center justify-center">
                 <div className="bg-gradient-to-b grain py-10 px-5 md:px-10 w-full rounded-4xl mt-10">
                     <motion.div
                         transition={{ type: "spring", stiffness: 100, velocity: 2 }}
-                        style={{
-                            marginLeft
-                        }}
-                       className="manrope text-sm md:text-3xl  text-center text-black pb-10">  Crafting Scalable & Modern Web Experiences</motion.div>
+                        // style={{
+                        //     marginLeft
+                        // }}
+                       className="manrope text-sm font-bold md:font-medium md:text-3xl  text-center text-black pb-10">  Crafting Scalable & Modern Web Experiences</motion.div>
                     <div className="text-white text-center spaicng tracking-wide md:tracking-normal lg:text-center ateoric text-[4rem] sm:text-[6.8rem] lg:text-[15rem] py-2 md:py-12 leading-[4rem] lg:leading-[11rem]">
                          <TextFade  direction="up"> Full-Stack Developer </TextFade>
                          </div>
                     <div className="w-full flex justify-between items-start gap-6 md:items-center">
                         <p className="text-sm text-start md:text-xl w-1/2 md:w-1/3 text-black  manrope">
-                            I build fast, accessible, and maintainable web applications with a focus on clean architecture and seamless user experiences.
+                            I build <b>fast,</b> accessible, and <b>maintainable,</b> web applications with a focus on <b>clean</b> architecture and design.
                         </p>
                         <p className="text-sm text-end md:text-xl w-1/2 md:w-1/3  text-black  manrope">
-                            With a deep understanding of modern frameworks and best practices, I bring ideas to life through code.        </p>
+                            With a <b>deep</b> understanding of modern frameworks and best practices, I bring ideas to life through code.        </p>
                     </div>
                 </div>
             </section>
