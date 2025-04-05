910c7d48a1988583dc1cfc83d7fec1bc6ca235ae
ayushsth123 <ayyush227@gmail.com>
2025-04-05 22:20:28 +0545

    fix

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Header = () => {
   const date = Date()
   return (
-    <section className="max-w-[1400px] fixed top-0  mt-5 md:mt-10 mx-auto w-full flex px-5 justify-between items-center">
+    <section className="w-full py-10 fixed flex px-5 justify-between items-center">
       <div>
         <div className='flex items-center gap-3'>
           {/* <Link href='/'>
@@ -17,12 +17,12 @@ const Header = () => {
               className='w-8 h-8 md:w-10 md:h-10' />
           </Link> */}
           <div>
-            <h1 className='nebula uppercase text-[10px] sm:text-sm md:text-lg  text-white'>Ayuxh</h1>
+            <h1 className='nebula uppercase text-[10px] sm:text-sm md:text-lg  text-black'>Ayuxh</h1>
             <div className='flex items-center gap-1 md:gap-1 '>
               <span className='text-green-400 text-lg animate-pulse'>▼</span>
-              <span className="relative manrope font-extralight uppercase text-[9px] md:text-sm text-white mt-1 md:mt-0 group">
+              <span className="relative manrope font-extralight uppercase text-[9px] md:text-sm text-black mt-1 md:mt-0 group">
                 Available for collaboration
-                <span className="absolute  left-0 bottom-0 w-full h-[1px] bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+                <span className="absolute  left-0 bottom-0 w-full h-[1px] bg-black scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
 
                 {/* <div className=' manrope leading-tight  uppercase font-extralight text-[12px] text-white'>
                   {date}
@@ -37,8 +37,8 @@ const Header = () => {
       </div>
 
       <div className='flex flex-col gap-1 group cursor-pointer'>
-        <div className='w-10 h-[3px] bg-white'></div>
-        <div className='w-10 h-[3px] bg-white group-hover:w-4 group-hover:duration-400'></div>
+        <div className='w-10 h-[3px] bg-black'></div>
+        <div className='w-10 h-[3px] bg-black group-hover:w-4 group-hover:duration-400'></div>
       </div>
     </section>
   )
